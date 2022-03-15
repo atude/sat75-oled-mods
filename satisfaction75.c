561af54c6524097b97e8bc6c74449115fd455518
Mozamel Anwary <mozamel.anwary1@gmail.com>
2022-03-16 01:30:35 +1100

    barebones

--- satisfaction75.c
+++ satisfaction75.c
@@ -23,7 +23,7 @@ uint8_t layer;
 
 bool clock_set_mode = false;
 uint8_t oled_mode = OLED_DEFAULT;
-uint8_t pet_mode = PET_LUNA;
+uint8_t pet_mode = PET_KIRBY;
 uint8_t bongo_mode = 0; // 0 = outline, 1 = filled
 uint8_t date_time_mode = 0; // 0 = time, 1 = date, 2 = none
 uint8_t timeout_mode = 0; // 0 = 1m30s, 1 = 2m, 2 = 5m, 3 = 1m
@@ -260,10 +260,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         break;
     case KC_F23:
         if (record->event.pressed) {
-            if (oled_mode == OLED_PETS) {
-                pet_mode = (pet_mode + 1) % _NUM_PET_MODES;
-                eeprom_update_byte((uint8_t*)EEPROM_ATUDE_PET_MODE, pet_mode);
-            } else if (oled_mode == OLED_BONGO) {
+            if (oled_mode == OLED_BONGO) {
                 bongo_mode = (bongo_mode + 1) % 2;
                 eeprom_update_byte((uint8_t*)EEPROM_ATUDE_BONGO_MODE, bongo_mode);
             }
@@ -271,10 +268,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         break;
     case KC_F24:
         if (record->event.pressed) {
-            if (oled_mode == OLED_PETS) {
-                pet_mode = (pet_mode - 1 < 0) ? _NUM_PET_MODES - 1 : (pet_mode - 1) % _NUM_PET_MODES;
-                eeprom_update_byte((uint8_t*)EEPROM_ATUDE_PET_MODE, pet_mode);
-            } else if (oled_mode == OLED_BONGO) {
+            if (oled_mode == OLED_BONGO) {
                 bongo_mode = (bongo_mode + 1) % 2;
                 eeprom_update_byte((uint8_t*)EEPROM_ATUDE_BONGO_MODE, bongo_mode);
             }
@@ -374,7 +368,6 @@ void custom_config_load(){
 #ifdef DYNAMIC_KEYMAP_ENABLE
   oled_mode = eeprom_read_byte((uint8_t*)EEPROM_ATUDE_OLED_MODE);
   bongo_mode = eeprom_read_byte((uint8_t*)EEPROM_ATUDE_BONGO_MODE);
-  pet_mode = eeprom_read_byte((uint8_t*)EEPROM_ATUDE_PET_MODE);
   date_time_mode = eeprom_read_byte((uint8_t*)EEPROM_ATUDE_DATETIME_MODE);
   timeout_mode = eeprom_read_byte((uint8_t*)EEPROM_ATUDE_TIMEOUT_MODE);
   enabled_encoder_modes = eeprom_read_byte((uint8_t*)EEPROM_ENABLED_ENCODER_MODES);
